9bb4313caee812b056d53d67efd8b144d4b9b383
hisduprojects <hisduteamb@gmail.com>
2023-06-23 09:24:17 +0500

    Z

--- OutsourcedServices-APP/src/app/app-routing.module.ts
+++ OutsourcedServices-APP/src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ import { AppLayoutComponent } from "./layout/app.layout.component";
                 
                 ]
             },
+            
             { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
             { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
             { path: 'notfound', component: NotfoundComponent },
